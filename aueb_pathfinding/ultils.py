2cfb490de8b11d1441ea9661600adee79539b6b1
Leo Ntrekos <l.ntrekos@gmail.com>
2025-12-24 21:39:56 +0200

    First steps for load_map()

--- aueb_pathfinding/ultils.py
+++ aueb_pathfinding/ultils.py
@@ -15,3 +15,10 @@ with open("aueb_map.txt", "r") as file:
 
 print("Aueb map from txt:")
 print(aueb_map)
+
+import re
+aueb_map.format()
+
+
+# I have to search what kind of object is the aueb_map 
+# and explore the re library
