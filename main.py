8f03f87142ff325f1742a8c78a5b9ef6e7996f99
Leo Ntrekos <l.ntrekos@gmail.com>
2026-01-04 14:34:48 +0200

    Inital programm structure

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from aueb_pathfinding.menu import (
-    menu
-)
-from aueb_pathfinding.classes import Classroom, University
+from aueb_pathfinding.menu import menu, load_map, create_graph
+from aueb_pathfinding.ultils import dijkstra, visualize_graph
 
-from aueb_pathfinding.ultils import (
-    clean_values, # is used inside load_map function
-    load_map, distance
-)
+aueb_map = None
+while True:
+    choice = menu()
 
-# Load txt file
-aueb_map = load_map()
+    if choice == 1:
+        aueb_map = load_map()
+        print("\nAUEB map loaded succesfully,\nreturning to main menu: ")
+    
+    elif choice == 2:
+        uni = create_graph(aueb_map)
+        if uni is not None:
+            print(uni)
+        print("\nReturning to main menu:")
 
-# Initialize University Class
-Aueb = University()
+    elif choice == 3:
+        #shortest_path, distance = dijkstra()
+        print("Find shortest path")
+    
+    elif choice == 4:
+        #fig = visualize_graph(uni.nodes, max_distance=uni.max_distance)
+        #print(fig)
+        print("VIz")
+            
+    elif choice == 5:
+        print("Exiting...")
+        break
 
-# add classes from txt file (aueb_map) inside the University class
-for name, x, y, floor in zip(aueb_map["classroom"],aueb_map["x"],aueb_map["y"],aueb_map["floor"]):
-    Aueb.add_node(Classroom(name=name, x=x, y=y, floor=floor))
-
-for src in Aueb.nodes:
-    for dest in Aueb.nodes:
-        Aueb.add_edge(src, dest)
-
-
-import networkx as nx
-Aueb_Graph = nx.Graph()
-for node in Aueb.nodes:
-    Aueb_Graph.add_node(node.name, pos = [node.x, node.y])
-print(Aueb_Graph)
-
-Aueb.edges["A21"]
-Aueb.edges["A22"]
